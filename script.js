2ce2e629e8f5c6ed9699ed14f2e5dfb4a970dd01
James Nolan <jamesn8391@gmail.com>
2023-01-29 02:49:25 -0600

    fixed folder hierarchy

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9500;
 
 // sendFile will go here
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
